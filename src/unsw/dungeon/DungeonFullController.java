6e604a14b461e77f26734e4216ce6f744a305b04
z5119615 <k.liyanto@unsw.edu.au>
2019-07-31 14:58:00 +1000

    Loaded dungeon with methods to unload the dungeon

--- src/unsw/dungeon/DungeonFullController.java
+++ src/unsw/dungeon/DungeonFullController.java
@@ -1,5 +1,6 @@
 package unsw.dungeon;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import javafx.event.ActionEvent;
@@ -8,6 +9,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -35,8 +38,8 @@ public class DungeonFullController {
 	@FXML
 	private Menu levelmenu;
 
-    List<String> dungeonPaths;
-	    
+    private List<String> dungeonPaths;
+	private Dungeon loadedDungeon = null;
 	    
     public DungeonFullController(List<String> dungeonPaths) {
     	// dungeonPaths will take all dungeons and make the required levels on the dungeon selector
@@ -52,24 +55,35 @@ public class DungeonFullController {
     		String rname = name.replaceAll(".json$", "");
     		System.out.println("here " + rname);
     		MenuItem r = new MenuItem(rname);
+    		r.setOnAction((event) -> {
+    			// Load the dungeon with the specified name
+    			try {
+					loadDungeon(name);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+    		});
     		levelmenu.getItems().add(r);
     	}
 	}
     
     @FXML
     void handleKeyPress(KeyEvent event) {
+    	if (loadedDungeon == null) return;
+    	
+    	Player player = loadedDungeon.getPlayer();
     	switch (event.getCode()) {
         case UP:
-            // player.moveUp();
+            player.moveUp();
             break;
         case DOWN:
-            // player.moveDown();
+            player.moveDown();
             break;
         case LEFT:
-            // player.moveLeft();
+            player.moveLeft();
             break;
         case RIGHT:
-            // player.moveRight();
+            player.moveRight();
             break;
         case R:
             // Restart the game
@@ -95,6 +109,44 @@ public class DungeonFullController {
 
     }
 
+    private void loadDungeon(String pathname) throws FileNotFoundException {
+    	clearDungeon();
+    	System.out.println(pathname);
+    	DungeonFullLoader dungeonLoader = new DungeonFullLoader(pathname);
+    	this.loadedDungeon = dungeonLoader.load();
+    	Dungeon dungeon = this.loadedDungeon;
+    	List<ImageView> initialEntities = dungeonLoader.getEntities();
+    	
+    	
+    	Image ground = new Image("/dirt_0_new.png");
+        Image blank = new Image("/blank.png");
+        Image backpack = new Image("/backpack.png");
 
+        // Add the ground first so it is below all other entities
+        for (int x = 0; x < dungeon.getWidth(); x++) {
+            for (int y = 0; y < dungeon.getHeight(); y++) {
+                squares.add(new ImageView(ground), x, y);
+            }
+        }
+        
+        squares.add(new ImageView(backpack), dungeon.getWidth(), 0);
+
+        for (int y = 1; y < dungeon.getHeight(); y++) {
+            int x = dungeon.getWidth();
+            squares.add(new ImageView(blank), x, y);
+        }
+
+        for (ImageView entity : initialEntities)
+            squares.getChildren().add(entity);
+        
+        pane.setMinSize(dungeon.getWidth(), dungeon.getHeight());
+        pane.setMaxSize(dungeon.getWidth(), dungeon.getHeight());
+        pane.setPrefSize(dungeon.getWidth(), dungeon.getHeight());
+    }
+    
+    private void clearDungeon() {
+    	this.loadedDungeon = null;
+    	squares.getChildren().clear();
+    }
 }
 
